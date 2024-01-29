b1bd33e9219c04df700ca6d1f9c923d51a748fc6
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-29 14:25:41 -0800

    Created Budget, ExpenseTotal and Remaining components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
 
 function App() {
   return (
-    <div className="App">
-     Hello World
+    <div className="container">
+      <h1 className="mt-3">
+        My Budget Planner
+      </h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget/>
+
+        </div>
+        <div className="col-sm">
+          <Remaining/>
+        </div>
+        <div className="col-sm">
+          <ExpenseTotal/>
+        </div>
+
+      </div>
+
+      <h3 className="mt-3">Expenses</h3>
     </div>
   );
 }
---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ExpenseTotal = () => {
+  return (
+      <div className='alert alert-primary'>
+          <span>
+              Spent so far : $960
+          </span>
+    </div>
+  )
+}
+
+export default ExpenseTotal
